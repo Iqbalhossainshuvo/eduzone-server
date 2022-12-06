462421d9e2b1f3c0bf6a9287a7dc67c824b3cadd
Iqbalhossainshuvo <mdiqbalhossain203050@gmail.com>
2022-12-06 10:25:14 +0600

    Add data add

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const cors = require('cors')
+app.use(cors())
+
+
+ const courses = require("./data/courses.json") 
+
+
+app.get('/courses',(req,res)=>{
+    res.send(courses)
+}) 
+ app.get('/courses/:id',(req,res)=>{
+  const id= req.params.id;
+  const coursesDetails = courses.find(c =>c.id===id)
+    res.send(coursesDetails)
+}) 
+
+
+
+ app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+ 
+
+
+ app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+}) 
